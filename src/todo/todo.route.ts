65cc813dca959d669a6aefe073ef22f51772bb70
Mohamed Mahfouz <mohamadmahfouz39@gmail.com>
2025-07-11 21:02:56 +0300

    Implement basic CRUD endpoints for todo API (#2)

--- src/todo/todo.route.ts
+++ src/todo/todo.route.ts
@@ -4,3 +4,7 @@ import {todoController} from "./todo.controller";
 export const todoRouter = express.Router();
 
 todoRouter.get("/", todoController.getAllTodos);
+todoRouter.get("/:id", todoController.getTodoById);
+todoRouter.post("/", todoController.createTodo);
+todoRouter.delete("/:id", todoController.deleteTodoById);
+todoRouter.put("/:id", todoController.updateTodoById);
