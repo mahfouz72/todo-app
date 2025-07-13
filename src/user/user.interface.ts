b89145cf52c76bca1d611ee7245ed3d13b10b93f
mahfouz72 <mohamadmahfouz39@gmail.com>
2025-07-13 15:11:32 +0300

    Infer user type from the zod schema

--- src/user/user.interface.ts
+++ src/user/user.interface.ts
@@ -1,4 +1,8 @@
-export interface User {
-    username: string,
-    password: string,
-}
+import {z} from 'zod';
+
+export const userSchema = z.object({
+    username: z.string(),
+    password: z.string().min(8, "Password must be at least 8 characters"),
+})
+
+export type User = z.infer<typeof userSchema>
