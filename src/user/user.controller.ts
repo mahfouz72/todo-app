26dbcb0dce3a51515e13a5b7bf788cfa0f3129e2
Mohamed Mahfouz <mohamadmahfouz39@gmail.com>
2025-07-14 19:45:51 +0300

    Setup mysql database and create simple operations on user table (#7)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,19 @@
-import {Request, Response} from "express";
-import {userStore} from "./user.model";
+import {NextFunction, Request, Response} from "express";
+import {userRepository} from "./user.model";
 import {User, userSchema} from "./user.interface";
 
 export const userController = {
-    getAllUsers: (req: Request, res: Response) => {
-        res.json(userStore.users);
+    getAllUsers: (req: Request, res: Response, next: NextFunction) => {
+        userRepository.getAllUsers().then(users => {
+            res.json(users);
+        }).catch(next);
     },
 
-    createUser: (req: Request, res: Response) => {
+    createUser: (req: Request, res: Response, next: NextFunction) => {
         const user : User = req.body;
         userSchema.parse(user);
-        userStore.users.push(user);
-        res.json(user);
+        userRepository.createUser(user).then(_ =>
+            res.json(user)
+        ).catch(next);
     }
 }
--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -1,5 +1,15 @@
 import {User} from "./user.interface";
+import dp from "../config/database.config";
 
-export const userStore = {
-    users: <User[]> []
+export const userRepository = {
+    getAllUsers: async ()  => {
+        const query = "SELECT * FROM `USER`";
+        const result = await dp.promise().query(query);
+        return result[0];
+    },
+
+    createUser: async (user: User) => {
+        const query = "INSERT INTO `USER` (username, password) VALUES (?, ?)";
+        await dp.promise().query(query, [user.username, user.password]);
+    }
 }
