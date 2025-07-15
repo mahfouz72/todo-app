40d678dfabd4bab7a16b39d09dc6087fb1b88333
Mohamed Mahfouz <mohamadmahfouz39@gmail.com>
2025-07-15 20:18:32 +0300

    Add full CRUD for user API with complete DB integration (#9)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,11 +9,34 @@ export const userController = {
         }).catch(next);
     },
 
+    getUserById: (req: Request, res: Response, next: NextFunction) => {
+        let id = +req.params.id;
+        userRepository.getUserById(id).then(user => {
+            res.json(user);
+        }).catch(next);
+    },
+
     createUser: (req: Request, res: Response, next: NextFunction) => {
         const user : User = req.body;
         userSchema.parse(user);
         userRepository.createUser(user).then(_ =>
             res.json(user)
         ).catch(next);
+    },
+
+    updateUser: (req: Request, res: Response, next: NextFunction) => {
+        let id = +req.params.id;
+        const updateUser : User = req.body;
+        userSchema.parse(updateUser);
+        userRepository.updateUser(updateUser, id).then(user => {
+                res.json(user)
+        }).catch(next);
+    },
+
+    deleteUser: (req: Request, res: Response, next: NextFunction) => {
+        let id = +req.params.id;
+        userRepository.deleteUserById(id).then(user => {
+            res.json(user);
+        }).catch(next);
     }
 }
